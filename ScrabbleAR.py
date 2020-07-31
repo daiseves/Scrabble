58a8a90b0c324d896b191741b548e85cdad1b69b
Lariña <33741424+daiseves@users.noreply.github.com>
2020-07-31 17:35:09 -0300

    Update ScrabbleAR.py

--- ScrabbleAR.py
+++ ScrabbleAR.py
@@ -1,44 +1,30 @@
 import PySimpleGUI as sg
-from Bag import Bag
-from Board import Board
-import random
-import AI as ai
-import Funciones as fun
-import Jugar as j
-import Configuracion as c
+from Funciones import Funciones as fun
+from Funciones import NuevoJuego as n
+from Funciones import Jugar as j
+from Funciones import Top10 as t
 
-sg.theme_background_color('#E5CEAC')
-cant_rondas=1
-nivel='Fácil'
-jugadores=[]
-bag=Bag()
 
-layout = [
-            [sg.Image(filename='Imagenes/ScrabbleWord.png', size=(1000,300),background_color='#E5CEAC')],
-            [sg.Text('NOMBRE:', font ='centaur 30', size=(13,1), justification='center',background_color='#E5CEAC'), sg.InputText(key='__nombre__',size=(30,1), font='Courier 30')],
-            [sg.Text("",background_color='#E5CEAC')],
-            [sg.Text("",background_color='#E5CEAC')],
-            [sg.Button('INICIAR', button_color=('white','saddlebrown'), font='centaur 30', pad=((100,0)), size=(13,1)), sg.Button('CONFIGURACIÓN', button_color=('white','saddlebrown'), font='centaur 30', size=(20,1))]
-            ]
-window = sg.Window('Bienvenido', layout, size=(1100,600), location=(200,60))
+sg.SetOptions(background_color='#E5CEAC', button_color=('white','saddlebrown'), element_background_color='#E5CEAC',  text_element_background_color='#E5CEAC')
+
+des = {'font':("Current",16), 'size':(16, 2)}
+
+layout = [[sg.Image(filename='Imagenes/ScrabbleWord.png', size=(1000,250))],[sg.Button('Nuevo Juego', **des), sg.Button('Cargar Juego', **des), sg.Button('Top 10', **des)]]
+
+window = sg.Window('Bienvenido al ScrabbleAR', layout, size=(900,400), element_justification='center')
 
 while True:
-    event, values = window.Read()
-    if event is None:
-        break
-    if event is 'INICIAR':
-        window.close()
-        nom=values['__nombre__']
-        jug=fun.instanciar_jugadores(bag, nom, jugadores)
-        random.shuffle(jug) 
-        jugador_actual=jug[0]
-        sg.popup('Es el turno del jugador: ' ,jugador_actual.get_name(), background_color='#FFE0A3', text_color='saddlebrown')
-        if nivel=='Medio':
-            sg.theme('Purple')
-        elif nivel=='Difícil':
-            sg.theme('DarkBlue13')
-        board=Board(bag, nivel)
-        j.jugar(jugador_actual, board, jug, nivel, cant_rondas)
-    if event is 'CONFIGURACIÓN':
-        nivel=c.configuracion()
+    event, values  = window.read()
+    if event is 'Nuevo Juego':
+        window.Close()
+        n.inicio()
+    if event == 'Top 10':
+        t.main()
+    if event is 'Cargar Juego':
+        window.Close()
+        carga=True
+        valor=fun.set_partida()
+        sg.popup('Partida cargada con éxito. ')
+        jugador_actual=valor[3][1] if(valor[3][0].get_name()=='PC') else valor[3][0]
+        j.jugar(valor[0], valor[1], valor[2], valor[3], jugador_actual, valor[4], valor[5], valor[6], carga, valor[7], valor[8], valor[9])
             
