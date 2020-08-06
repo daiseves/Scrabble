411c6a9393caaea765686c48526e23956038d6f9
Lariña <33741424+daiseves@users.noreply.github.com>
2020-08-05 22:17:48 -0300

    Update AI.py

--- Funciones/AI.py
+++ Funciones/AI.py
@@ -1,3 +1,12 @@
+'''
+ScrabbleAR
+ - Autor: Remorini Maria Lara
+ - Mail: remoriniml@gmail.com
+ - GitHub: https://github.com/daiseves - 
+
+Todas las imagenes utilizadas en este proyecto son de propia autoría.
+'''
+
 import random
 import itertools
 import PySimpleGUI as sg
@@ -15,15 +24,14 @@ def combinaciones_pc(ficha_centro, jugador, cant_rondas):
     
     """
     
-    
-    letras=jugador.atril_array()
+    letras=jugador.atril_array().copy()
     combinaciones=[]
     if cant_rondas==1:
         letras.append(ficha_centro)
-    for p in range(2, 9):
+    for p in range(2, 5):       #rango de 2 a 4 letras, si quiero las combinaciones completas, cambio a range(2,9)
         lista_de_letras= list(itertools.permutations(list(letras), p))
         combinaciones.append(lista_de_letras)
-    return combinaciones[1]     #Devuelve sólo las combinaciones de tres letras. (2: cuatro letras. 3: cuatro letras....ó, sin índice, todas)
+    return combinaciones    
     
 
 
@@ -33,26 +41,33 @@ def generador_de_palabra(ficha_centro, jugador, board, cant_rondas, dicTablero, 
     Función que recibe una lista de combinaciones posibles y toma la primer palabra verificada como válida.
     
     """
-    
+    j=2    #Combinaciones de tantas letras. 2 =  combinaciones de cuatro letras
     found=True
-    i=0
     combinaciones=combinaciones_pc(ficha_centro, jugador, cant_rondas)
-    while found and i<len(combinaciones):
-        if combinaciones!=None :
-            word = "".join(combinaciones[i])
-            palabra=Palabra(word, jugador, board, bag)
-            if cant_rondas==1 or len(dicTablero)==1:
-                if ficha_centro in word:
+    while found and j>=0:
+        print(j)
+        i=0
+        while found and i<len(combinaciones[j]):
+            print(combinaciones[j])
+            if combinaciones!=None :
+                word = "".join(combinaciones[j][i])
+                palabra=Palabra(word, jugador, board, bag)
+                if cant_rondas==1 or len(dicTablero)==1:
+                    if ficha_centro in word:
+                        if palabra.convert():
+                            found=False
+                            return word, palabra
+                else:
                     if palabra.convert():
-                        found=False
+                        found= False
                         return word, palabra
-                
-            else:
-                if palabra.convert():
-                    found= False
-                    return word, palabra
-            i=i+1
-    if i==(len(combinaciones)):
+                i=i+1
+        print(found)
+        j=j-1     
+    
+    #Si llega al valor de len(combinaciones), significa que no encontró la palabra
+    if found:
+        print('entra')
         word=None
         return word, False
             
@@ -65,11 +80,17 @@ def posiciona_palabra(palabra, ficha_centro, dicTablero, board, jugador, window,
     
     """
     
+    if nivel=='Facil':
+        limite=15
+    elif nivel=='Medio':
+        limite=17
+    elif nivel=='Dificil':
+        limite=19
+        
     f=board.get_medio()[0]
     c=f
     if cant_rondas==1 or len(dicTablero)==1:
         dicc[(f, c)]=ficha_centro
-        #Recibo la posición de la letra que coincide con la ficha central para hacer el continue y poder posicionar la palabra de forma correcta sin superponer las fichas.
         vp=valor_posicion(palabra, ficha_centro)
         for i in range(len(palabra)):
             if i==vp:
@@ -84,19 +105,19 @@ def posiciona_palabra(palabra, ficha_centro, dicTablero, board, jugador, window,
         exito=False
         while not exito:
             pos=random.choice([0,1])
-            f=(random.randint(0, 14))
-            c=(random.randint(0, 14))
+            f=(random.randint(0, limite-1))
+            c=(random.randint(0, limite-1))
 
             modo=random.choice([0,1])   #0: vertical , 1:horizontal
 
             #Verifico si la palabra entra en el tablero
             if modo==0:  
-                if (f+len(palabra)>=15):    
-                    a = f+len(palabra)-15
+                if (f+len(palabra)>=limite):    
+                    a = f+len(palabra)-limite
                     f = f-a
             elif modo==1:
-                if (c+len(palabra)>=15):
-                    a = c+len(palabra)-15
+                if (c+len(palabra)>=limite):
+                    a = c+len(palabra)-limite
                     c = c-a
 
             #Verifico que la palabra se pueda poner en el tablero
@@ -117,6 +138,7 @@ def posiciona_palabra(palabra, ficha_centro, dicTablero, board, jugador, window,
                 hayEspacio=False
                 
                 
+            # HAY PROBLEMA SI NO HAY ESPACIO HAY QUEPONERLE UN TRY EXCEPT
             if hayEspacio:
                 fila=key[0]
                 columna=key[1]
@@ -145,7 +167,25 @@ def valor_posicion(lista, ficha_central):
         i=i+1
     return i 
         
-            
+ 
+
+def cambia_fichasAI(window, jugador):
+    """
+    Función que cambias las fichas del atril de la PC en caso de que no encuentre combinación de palabras.
+    
+    """
+    azar=[0,1,2,3,4,5,6]
+    print(jugador.atril_array())
+    letras_a_cambiar=[]
+    for i in range(3):
+        num=random.choice(azar)
+        azar.remove(num)
+        print(num)
+        letras_a_cambiar.append(jugador.atril_array()[num])
+    jugador.cambiar_fichas(letras_a_cambiar)
+    fun.completo_atril(window, jugador)
+    
+    
                               
 def juega_PC(dicc,dicTablero, ficha_central, jugador, board, window, jugadores, cant_rondas, multiUser, multiPC, ultimaPalabra, nivel, ultima_palabra, bag, lista_coordenadas):
        
